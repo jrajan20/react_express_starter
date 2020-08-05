8be6088b82e45146cac481598a371e21047f5699
Jai Rajan <jai_20j@hotmail.com>
2020-08-05 09:06:34 -0400

    added currency exchange

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -8,11 +8,36 @@ class Account extends Component {
       withdrawal: null,
       deposit: null,
       transfer: null,
+      transferAcc: null,
+      wCurrency: null,
+      dCurrency: null,
+      tCurrency: null,
     };
   }
 
+  handleWithdrawCurrency = (evt) => {
+    this.setState({ wCurrency: evt.target.value });
+  };
+
+  handleDepositCurrency = (evt) => {
+    this.setState({ dCurrency: evt.target.value });
+  };
+
+  handleTransferCurrency = (evt) => {
+    this.setState({ tCurrency: evt.target.value });
+  };
+
   render() {
     const { account, back } = this.props;
+    const {
+      withdrawal,
+      deposit,
+      transfer,
+      wCurrency,
+      dCurrency,
+      tCurrency,
+      transferAcc,
+    } = this.state;
 
     return (
       <div>
@@ -29,48 +54,187 @@ class Account extends Component {
           <div className="account-title">{`Account Number: ${account.number}`}</div>
 
           <div className="action-card">
-            <div className="action-title">Withdraw</div>
+            <div className="action-title">Withdraw:</div>
             <div>
-              <input
-                className="amount-input"
-                type="number"
-                onChange={(e) => {
-                  this.setState({ withdrawal: e.target.value });
-                }}
-              />
-              <button className="confirm-button">Confirm</button>
+              <label>
+                <p>Amount:</p>${" "}
+                <input
+                  className="amount-input"
+                  type="number"
+                  onChange={(e) => {
+                    this.setState({ withdrawal: e.target.value });
+                  }}
+                />
+              </label>
+              <button
+                className="confirm-button"
+                disabled={!wCurrency || !withdrawal}
+              >
+                Confirm
+              </button>
+            </div>
+            <div className="currency-selection">
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value="CAD"
+                    checked={this.state.wCurrency === "CAD"}
+                    onChange={this.handleWithdrawCurrency}
+                  />
+                  CAD
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value="USD"
+                    checked={this.state.wCurrency === "USD"}
+                    onChange={this.handleWithdrawCurrency}
+                  />
+                  USD
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value="MXN"
+                    checked={this.state.wCurrency === "MXN"}
+                    onChange={this.handleWithdrawCurrency}
+                  />
+                  MXN
+                </label>
+              </div>
             </div>
           </div>
           <div className="action-card">
             <div className="action-title">Deposit:</div>
             <div>
-              <input
-                className="amount-input"
-                type="number"
-                onChange={(e) => {
-                  this.setState({ deposit: e.target.value });
-                }}
-              />
-              <button className="confirm-button">Confirm</button>
+              <label>
+                <p>Amount:</p>${" "}
+                <input
+                  className="amount-input"
+                  type="number"
+                  onChange={(e) => {
+                    this.setState({ deposit: e.target.value });
+                  }}
+                />
+              </label>
+              <button
+                className="confirm-button"
+                disabled={!dCurrency || deposit}
+              >
+                Confirm
+              </button>
+              <div className="currency-selection">
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      value="CAD"
+                      checked={this.state.dCurrency === "CAD"}
+                      onChange={this.handleDepositCurrency}
+                    />
+                    CAD
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      value="USD"
+                      checked={this.state.dCurrency === "USD"}
+                      onChange={this.handleDepositCurrency}
+                    />
+                    USD
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      value="MXN"
+                      checked={this.state.dCurrency === "MXN"}
+                      onChange={this.handleDepositCurrency}
+                    />
+                    MXN
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
           <div className="action-card">
-            <div className="action-title">Transfer</div>
+            <div className="action-title">Transfer:</div>
             <div>
-              <input
-                className="amount-input"
-                type="number"
-                onChange={(e) => {
-                  this.setState({ transfer: e.target.value });
-                }}
-              />
-              <button className="confirm-button">Confirm</button>
+              <label>
+                <p>To Account #:</p>
+                <input
+                  className="amount-input"
+                  type="number"
+                  onChange={(e) => {
+                    this.setState({ transferAcc: e.target.value });
+                  }}
+                />
+              </label>
+              <label>
+                <p>Amount:</p>${" "}
+                <input
+                  className="amount-input"
+                  type="number"
+                  onChange={(e) => {
+                    this.setState({ transfer: e.target.value });
+                  }}
+                />
+              </label>
+
+              <button
+                className="confirm-button"
+                disabled={!tCurrency || !transferAcc || !transfer}
+              >
+                Confirm
+              </button>
+              <div className="currency-selection">
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      value="CAD"
+                      checked={this.state.tCurrency === "CAD"}
+                      onChange={this.handleTransferCurrency}
+                    />
+                    CAD
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      value="USD"
+                      checked={this.state.tCurrency === "USD"}
+                      onChange={this.handleTransferCurrency}
+                    />
+                    USD
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      value="MXN"
+                      checked={this.state.tCurrency === "MXN"}
+                      onChange={this.handleTransferCurrency}
+                    />
+                    MXN
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="action-card">
-            <div>Transaction History</div>
-            <div className="history-box"></div>
-          </div>
+
+          <div className="action-title">Transaction History</div>
+          <div className="history-box"></div>
         </div>
       </div>
     );
